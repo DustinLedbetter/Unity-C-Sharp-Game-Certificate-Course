da581581d600d2e3ebfcaf5ea65d44c8b27945f2
Dustin Ledbetter <dustinledbetter9@gmail.com>
2017-07-25 14:10:05 -0400

    Add files via upload

---
+++ CS Practice Files/MyScriptVariables.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour {
+
+	/*
+
+	comment 1 multi
+	 
+	*/
+
+
+	//comment 2 single
+
+	//variables
+	float speed = 2.3f;
+	double power = 4.2;
+	int health = 23;
+	string name = "Elf";
+	bool isOn = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
